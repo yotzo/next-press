929e67fd80cf4490d0931f2034c24339ae68eb71
Yordan Aleksiev <yordan.s.aleksiev@gmail.com>
2026-03-31 18:57:35 +0300

    fixx typing and sorting issues in shadcn components

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,7 +1,6 @@
 import { CheckIcon } from "lucide-react";
 import { Checkbox as CheckboxPrimitive } from "radix-ui";
 import type * as React from "react";
-
 import { cn } from "#/lib/utils";
 
 function Checkbox({
